0377837479e0fe01bf9b240813dae06f27b01dd9
OCurze <38127010+OCurze@users.noreply.github.com>
2018-04-06 10:29:14 +0200

    Update Tankmodell.py

--- Tankmodell.py
+++ Tankmodell.py
@@ -1,2 +1,19 @@
 '''Tankmodell av Gode Tanker'''
+
+# Importering av nødvendige biblioteker
+
 import matplotlib.pyplot as plt
+import math
+
+# Variabler
+
+# Initialverdier 
+
+# Modell
+
+def euler(h, dh, dt):
+  return h + dh * dt
+
+def stigning(A_h, A_t, h, q_inn):
+  return 1/A_t * (q_inn - A_h * k * math.sqrt(h))
+
