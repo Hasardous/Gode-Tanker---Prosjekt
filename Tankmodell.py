349f24ca7026d2005e0f1e0352d1819b22f0e8aa
OCurze <38127010+OCurze@users.noreply.github.com>
2018-04-06 10:57:35 +0200

    Update Tankmodell.py

--- Tankmodell.py
+++ Tankmodell.py
@@ -7,7 +7,24 @@ import math
 
 # Variabler
 
+r1 = 0.135
+r2 = 0.135
+A_t1 = math.pi() * r1^2
+A_t2 = math.pi() * r2^2
+max_h1 = 0.25
+max_h2 = 0.25
+A_h1 = 0 #todo
+A_h2 = 0 #todo
+g = 9.81  
+q_inn1 = 0 #todo
+C = 0.61
+k = C * math.sqrt(2*g)
+
 # Initialverdier 
+
+h_1 = 0.20
+h_2 = 0.20
+q_inn2 = 0
 
 # Modell
 
@@ -17,3 +34,36 @@ def euler(h, dh, dt):
 def stigning(A_h, A_t, h, q_inn):
   return 1/A_t * (q_inn - A_h * k * math.sqrt(h))
 
+def hastighet(h):
+  if h > 0.0:
+    retrurn k * math.sqrt(h)
+  else:
+    returnt 0.0
+
+# Program
+
+while h_1 > h_lim and t < t_lim:
+  if t > 0: 
+    h_1 = euler(h_1, stigning(A_h1, A_t1, h_1, q_inn1), dt)
+    v_1 = hastighet(h_1) 
+    q_inn2 = A_h1 * v_1 
+    h_2 = euler(h_2, stigning(A_h2, A_t2, h_2, q_inn2), dt)
+    v_2 = hastighet(h_2)
+  
+  # print("t = {} => h_1 = {} \t q_inn1 = {} \t h_2 = {} \t q_inn2 = {}".format(t, round(h_1,2), q_inn1, round(h_2,2), round(q_inn2,4)))
+  t_hist.append(t)
+  h1_hist.append(h_1)
+  h2_hist.append(h_2)
+  t += dt
+
+# Plotting
+
+plt.plot(t_hist, h1_hist)
+plt.plot(t_hist, h2_hist)
+plt.ylabel('Høyde [m]')
+plt.xlabel('Tid [s]')
+plt.axis([0, t_lim, min(h1_hist)*0.9, max(h1_hist)*1.1])
+plt.grid()
+plt.show()
+    
+    
