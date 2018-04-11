025e9b5acc92c27adf2f24d01467975bcd7253fd
Hasardous <hjbamle@gmail.com>
2018-04-11 19:35:06 +0200

    Endring av indentation
    Indentation var merkelig. Fikset dette og satte også opp tegningen av tankene med en bedre indentation
    
    Spørsmål hvis noen leser denne:
       Skal jeg flytte fargene i blokken slik at stilen blir helt fulgt eller beholde de to småverdiene på en linje?

--- Animasjon.py
+++ Animasjon.py
@@ -27,68 +27,69 @@ timer_font = pygame.font.SysFont('Consolas', 30)
 # Tankenes parametre i et dictionary
 # TODO HAR IKKE NOEN REELLE TALL
 tank_1_params = {
-  "LEFT": CENTER_HORIZONTAL - 175,
-  "TOP": CENTER_VERTICAL - 125,
-  "WIDTH": 150,
-  "HEIGHT": 250,
-  "FILL_COLOUR": BLUE,
-  "OUTLINE_COLOUR": WHITE,
-  "BORDER_WIDTH": 3
+    "LEFT": CENTER_HORIZONTAL - 175,
+    "TOP": CENTER_VERTICAL - 125,
+    "WIDTH": 150,
+    "HEIGHT": 250,
+    "FILL_COLOUR": BLUE,
+    "OUTLINE_COLOUR": WHITE,
+    "BORDER_WIDTH": 3
 }
 
 tank_2_params = {
-  "LEFT": CENTER_HORIZONTAL + 25,
-  "TOP": CENTER_VERTICAL - 125,
-  "WIDTH": 150,
-  "HEIGHT": 250,
-  "FILL_COLOUR": BLUE,
-  "OUTLINE_COLOUR": WHITE,
-  "BORDER_WIDTH": 3
+    "LEFT": CENTER_HORIZONTAL + 25,
+    "TOP": CENTER_VERTICAL - 125,
+    "WIDTH": 150,
+    "HEIGHT": 250,
+    "FILL_COLOUR": BLUE,
+    "OUTLINE_COLOUR": WHITE,
+    "BORDER_WIDTH": 3
 }
 
 # TESTKJØRING
 while True:
-   for event in pygame.event.get():
-      if event.type == pygame.QUIT: sys.exit()
-   
-   # Fyll in skjermen
-   screen.fill(BLACK)
-  
-   # Tegner opp innholdet i tankene (konstant halvfull)
-   pygame.draw.rect(screen, BLUE, 
-                    pygame.Rect(tank_1_params["LEFT"], 
-                                tank_1_params["TOP"] + tank_1_params["HEIGHT"]/2, 
-                                tank_1_params["WIDTH"], 
-                                tank_1_params["HEIGHT"]/2))
-  
-   pygame.draw.rect(screen, BLUE, 
-                    pygame.Rect(tank_2_params["LEFT"], 
-                                tank_2_params["TOP"] + tank_2_params["HEIGHT"]/2, 
-                                tank_2_params["WIDTH"], 
-                                tank_2_params["HEIGHT"]/2))
-  
-   # Tegner opp omrisset til tankene
-   pygame.draw.rect(screen, WHITE, 
-                    pygame.Rect(tank_1_params["LEFT"], 
-                                tank_1_params["TOP"], 
-                                tank_1_params["WIDTH"], 
-                                tank_1_params["HEIGHT"]),
-                                tank_1_params["BORDER_WIDTH"])
-  
-   pygame.draw.rect(screen, WHITE, 
-                    pygame.Rect(tank_2_params["LEFT"], 
-                                tank_2_params["TOP"], 
-                                tank_2_params["WIDTH"], 
-                                tank_2_params["HEIGHT"]),
-                                tank_2_params["BORDER_WIDTH"])
-   
-   #Timer på toppen
-   timer_string = "Runtime: {} seconds".format(round(pygame.time.get_ticks() / 1000, 1))
-   timer = timer_font.render(timer_string, True, WHITE)
-   timer_rect = timer.get_rect()
-   timer_rect.center = (CENTER_HORIZONTAL), (CENTER_VERTICAL - 175)
-   screen.blit(timer, timer_rect)
-   
-   #Skjermoppdatering
-   pygame.display.flip()
-   clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+
+    # Fyll in skjermen
+    screen.fill(BLACK)
+
+    # Tegner opp innholdet i tankene (konstant halvfull)
+    pygame.draw.rect(screen, BLUE,
+                     pygame.Rect(tank_1_params["LEFT"],
+                                 tank_1_params["TOP"] + tank_1_params["HEIGHT"]/2,
+                                 tank_1_params["WIDTH"],
+                                 tank_1_params["HEIGHT"]/2))
+
+    pygame.draw.rect(screen, BLUE,
+                     pygame.Rect(tank_2_params["LEFT"],
+                                 tank_2_params["TOP"] + tank_2_params["HEIGHT"]/2,
+                                 tank_2_params["WIDTH"],
+                                 tank_2_params["HEIGHT"]/2))
+
+    # Tegner opp omrisset til tankene
+    pygame.draw.rect(screen, WHITE,
+                     pygame.Rect(tank_1_params["LEFT"],
+                                 tank_1_params["TOP"],
+                                 tank_1_params["WIDTH"],
+                                 tank_1_params["HEIGHT"]),
+                     tank_1_params["BORDER_WIDTH"])
+
+    pygame.draw.rect(screen, WHITE,
+                     pygame.Rect(tank_2_params["LEFT"],
+                                 tank_2_params["TOP"],
+                                 tank_2_params["WIDTH"],
+                                 tank_2_params["HEIGHT"]),
+                     tank_2_params["BORDER_WIDTH"])
+
+    #Timer på toppen
+    timer_string = "Runtime: {} seconds".format(round(pygame.time.get_ticks() / 1000, 1))
+    timer = timer_font.render(timer_string, True, WHITE)
+    timer_rect = timer.get_rect()
+    timer_rect.center = (CENTER_HORIZONTAL), (CENTER_VERTICAL - 175)
+    screen.blit(timer, timer_rect)
+
+    #Skjermoppdatering
+    pygame.display.flip()
+    clock.tick(FPS)
