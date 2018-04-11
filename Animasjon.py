aee475eb8d844c76ffc8d9abfc300d593ac4cf45
Hasardous <hjbamle@gmail.com>
2018-04-11 13:23:37 +0200

    Kopierer fra Tankanimasjonen til TJ og Rune
    Bare kopiering

--- Animasjon.py
+++ Animasjon.py
@@ -8,8 +8,20 @@ import pygame
 # Starter opp PyGame
 pygame.init()
 
+# Definerer farger
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 BLUE = (0, 105, 148)
 SEA_BLUE = (0,105,148)
+
+# Definerer Vinduet og noen posisjoner
+SIZE = WIDTH, HEIGHT = 640, 480
+screen = pygame.display.set_mode(SIZE)
+CENTER_HORIZONTAL = WIDTH // 2
+CENTER_VERTICAL = HEIGHT // 2
+
+# Timer
+FPS = 10
+timer = pygame.time.Clock()
+timer_text = pygame.font.SysFont('Consolas', 30)
 
