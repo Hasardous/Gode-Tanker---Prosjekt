89e09e4b70acf9dacb12a9d2645b3682110e7806
Hasardous <hjbamle@gmail.com>
2018-04-11 13:57:41 +0200

    Enda en endring

--- Animasjon.py
+++ Animasjon.py
@@ -27,8 +27,8 @@ timer_text = pygame.font.SysFont('Consolas', 30)
 # Tankenes parametre i et dictionary
 # TODO HAR IKKE NOEN REELLE TALL
 tank_1_params = {
-  "LEFT": CENTER_HORIZONTAL - 25,
-  "TOP": CENTER_VERTICAL - 25,
+  "LEFT": CENTER_HORIZONTAL - 100,
+  "TOP": CENTER_VERTICAL,
   "WIDTH": 150,
   "HEIGHT": 250,
   "FILL_COLOUR": BLUE,
@@ -37,8 +37,8 @@ tank_1_params = {
 }
 
 tank_2_params = {
-  "LEFT": CENTER_HORIZONTAL + 50,
-  "TOP": CENTER_VERTICAL + 50,
+  "LEFT": CENTER_HORIZONTAL + 100,
+  "TOP": CENTER_VERTICAL,
   "WIDTH": 150,
   "HEIGHT": 250,
   "FILL_COLOUR": BLUE,
