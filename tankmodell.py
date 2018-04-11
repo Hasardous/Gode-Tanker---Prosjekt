39f6c71e45789e099abdb701f48d218f643baa54
OCurze <38127010+OCurze@users.noreply.github.com>
2018-04-11 12:55:12 +0200

    Update tankmodell.py

--- tankmodell.py
+++ tankmodell.py
@@ -11,16 +11,16 @@ r_1 = 0.135
 r_2 = 0.135
 A_T1 = math.pi() * r_1^2
 A_T2 = math.pi() * r_2^2
-MAX_H1 = 0.25
-MAX_H2 = 0.25
-A_HULL1 = 0 #todo
-A_HULL2 = 0 #todo
+max_h1 = 0.25
+max_h2 = 0.25
+A_hull1 = 0 #todo
+A_hull2 = 0 #todo
 g = 9.81
 q_inn_1 = 0 #todo
 C = 0.61
 k = C * math.sqrt(2*g)
 d_t = 60
-MIN_H = 0.01
+min_h = 0.01
 max_t = 4800
 
 # Initialverdier
@@ -50,29 +50,29 @@ def hastighet(h):
 # Program
 
 t_hist = []
-H1_HIST = []
-H2_HIST = []
+h1_hist = []
+h2_hist = []
 
-while h_1 > MIN_H and t < max_t:
+while h_1 > min_h and t < max_t:
   if t > 0:
-    h_1 = euler(h_1, stigning(A_HULL1, A_T1, h_1, q_inn_1), d_t)
+    h_1 = euler(h_1, stigning(A_hull1, A_T1, h_1, q_inn_1), d_t)
     v_1 = hastighet(h_1)
-    q_inn_2 = A_HULL1 * v_1
-    h_2 = euler(h_2, stigning(A_HULL2, A_T2, h_2, q_inn_2), d_t)
+    q_inn_2 = A_hull1 * v_1
+    h_2 = euler(h_2, stigning(A_hull2, A_T2, h_2, q_inn_2), d_t)
     v_2 = hastighet(h_2)
 
   # print("t = {} => h_1 = {} \t q_inn_1 = {} \t h_2 = {} \t q_inn_2 = {}".format(t, round(h_1,2), q_inn_1, round(h_2,2), round(q_inn_2,4)))
   t_hist.append(t)
-  H1_HIST.append(h_1)
-  H2_HIST.append(h_2)
+  h1_hist.append(h_1)
+  h2_hist.append(h_2)
   t += d_t
 
 # Plotting
 
-plt.plot(t_hist, H1_HIST)
-plt.plot(t_hist, H2_HIST)
+plt.plot(t_hist, h1_hist)
+plt.plot(t_hist, h2_hist)
 plt.ylabel('Høyde [m]')
 plt.xlabel('Tid [s]')
-plt.axis([0, max_t, min(H1_HIST)*0.9, max(H1_HIST)*1.1])
+plt.axis([0, max_t, min(h1_hist)*0.9, max(h1_hist)*1.1])
 plt.grid()
 plt.show()
